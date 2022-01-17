163a02a4a386991e2b1473e4f3517ab01b53cfc2
Jay Modi <jay.modi105@gmail.com>
2022-01-17 18:11:13 +1000

    Install and configure truffle, add dependencies and restructure the code

---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            // Match any network id
+            network_id: "*"
+        },
+    },
+    // Place everything under React's src folder
+    contracts_directory: './src/contracts/',
+    contracts_build_directory: "./src/build",
+    migrations_directory: './src/migrations/',
+    compilers: {
+        solc: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    }
+}
