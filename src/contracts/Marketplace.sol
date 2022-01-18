e8ccc2843e51bf822c09b5adfd6f1ae3a3fb6251
Jay Modi <jay.modi105@gmail.com>
2022-01-18 20:15:11 +1000

    Create a product and add tests for it

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -4,8 +4,65 @@ pragma solidity ^0.8.0;
 contract Marketplace {
     // State variables
     string public name;
-    
+    uint public counter = 0; 
+
+    // Equivalent to database
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+
+    // Similar to declaring PK for adding data
+    mapping(uint => Product) public products;
+
+    // Emit event when a product is created
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
+
     constructor() {
-        name = "Jay Modi";
+        name = "Exotique Marketplace";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        // Check for valid params
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+
+        // Update Counter
+        counter ++;
+
+        // Create a product
+        products[counter] = Product(counter, _name, _price, msg.sender, false);
+
+        // Trigger an event (Similar to return)
+        emit ProductCreated(counter, _name, _price, msg.sender, false);
     }
 }
+
+
+/*
+Extra Notes:
+    1) Why counter?
+    Solidity doesn't tell us how many products are in Struct. It returns empty vals if you've 5 prods and search for 6. 
+
+    2) What is public variable?
+    Using public converts vaiable into function which can be used globally including console. More like auto increment PK.
+
+    3) Why to trigger event?
+    In Laravel, we return some value in the function. In solidity, we can trigger an event which will be passed as an argument in the callback of this function.
+
+    4) What is require() in a function?
+    The function will throw an exception and will stop the execution if the condition is not correct in order to save gas fee.
+
+    5) Why _ on parameters?
+    _ is just for naming convention to differentiate local variables from state variables. 
+*/
+
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,15 +1,21 @@
 /* eslint-disable no-undef */
 const Marketplace = artifacts.require("Marketplace");
 
-contract(Marketplace, (accounts) => {
+// Required for Failure tests (should.be.rejected)
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract(Marketplace, ([deployer, seller, buyer]) => {
     let marketplace
 
     before(async() => {
+        accounts = await web3.eth.getAccounts();
         marketplace = await Marketplace.deployed()
     })
 
-    describe('Marketplace Test', () => {
-        it('deploys successfully', async() => {
+    describe('Marketplace', () => {
+        it('deployed successfully', async () => {
             const address = await marketplace.address
 
             assert.notEqual(address, 0x0)
@@ -18,14 +24,42 @@ contract(Marketplace, (accounts) => {
             assert.notEqual(address, undefined)
         })
 
-        it('has a name', async() => {
+        it('has a name', async () => {
             const name = await marketplace.name()
 
-            assert.equal(name, 'Jay Modi')
+            assert.equal(name, 'Exotique Marketplace')
             assert.notEqual(name, 0x0)
             assert.notEqual(name, '')
             assert.notEqual(name, null)
             assert.notEqual(name, undefined)
         })
+    })
+
+    describe('Products', () => {
+        let result, productCount
+
+        before(async () => {
+            result = await marketplace.createProduct('iPhone X', web3.utils.toWei('1', 'Ether'), { from: seller })
+            productCount = await marketplace.counter()
+        })
+
+        // SUCCESS
+        it('created successfully', async () => {
+            const event = result.logs[0].args
+
+            assert.equal(productCount, 1)            
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'iPhone X', 'name is correct')
+            assert.equal(event.price, '1000000000000000000', 'price is correct')
+            assert.equal(event.owner, seller, 'owner is correct')
+            assert.equal(event.purchased, '', 'purchased is correct')
+        })
+
+        // FAILURE
+        it('not created successfully', async () => {
+            // These tests will fail because in the contract, we've require() for these params 
+            await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected 
+            await marketplace.createProduct('iPhone X', 0, { from: seller }).should.be.rejected 
+        })   
     })
 })
